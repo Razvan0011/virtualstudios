8f8c9255dd70a3b3cd51c74d678bd26528287728
Razvan <razvan.idnc@gmail.com>
2021-01-19 22:10:41 +0200

    New Commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const app = 1;
+const closeBtn = document.querySelectorAll(".close-btn");
+const hamburger = document.querySelectorAll(".hamburger");
+const smalldevice = document.querySelector(".overlay-menu");
+
+// hamburger[0].addEventListener("click", () => {
+//     smalldevice.classList.remove("hide");
+// })
+
+(() => {
+    hamburger.forEach(elem => {
+        elem.addEventListener("click", () => {
+            smalldevice.classList.remove("hide");
+        });
+    })
+})();
+
+(() => {
+    closeBtn.forEach(elem => {
+        elem.addEventListener("click", () => {
+            smalldevice.classList.add("hide");
+        });
+    })
+})();
